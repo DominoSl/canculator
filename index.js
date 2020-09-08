f23f2da0e627906ed1983996242ac9039df6caf9
DominoSl <artemenko.mik@gmail.com>
2020-09-08 13:12:04 +0300

    tryin hard

---
+++ index.js
@@ -0,0 +1,63 @@
+calc.one.addEventListener('click', function() {
+    calc.result.value += '1';
+}, false)
+
+calc.two.addEventListener('click', function() {
+    calc.result.value += '2';
+}, false)
+
+calc.three.addEventListener('click', function() {
+    calc.result.value += '3';
+}, false)
+
+calc.four.addEventListener('click', function() {
+    calc.result.value += '4';
+}, false)
+
+calc.five.addEventListener('click', function() {
+    calc.result.value += '5';
+}, false)
+
+calc.six.addEventListener('click', function() {
+    calc.result.value += '6';
+}, false)
+
+calc.seven.addEventListener('click', function() {
+    calc.result.value += '7';
+}, false)
+
+calc.eight.addEventListener('click', function() {
+    calc.result.value += '8';
+}, false)
+
+calc.nine.addEventListener('click', function() {
+    calc.result.value += '9';
+}, false)
+
+calc.plus.addEventListener('click', function(){
+    calc.result.value *= '*';
+}, false)
+
+calc.div.addEventListener('click', function(){
+    calc.result.value /= '/';
+}, false)
+
+calc.plus.addEventListener('click', function(){
+    calc.result.value -= '-';
+}, false)
+
+calc.plus.addEventListener('click', function(){
+    calc.result.value += '+';
+}, false)
+
+calc.plus.addEventListener('click', function(){
+    calc.result.value += '+';
+}, false)
+
+calc.AC.addEventListener('click', function(){
+    calc.result.value = '';
+}, false)
+
+calc.equal.addEventListener('click', function() {
+    calc.result.value = eval(calc.result.value);
+}, false)
